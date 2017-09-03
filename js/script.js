0d6e4a380ab65da26afaae9e850eaaeeac99f685
Igor <creosotte@gmail.com>
2017-09-03 19:00:04 +0300

    Made quotes slider responsive

--- js/script.js
+++ js/script.js
@@ -4,14 +4,20 @@ $(document).ready(function(){
     items:1,
     dotsContainer: '#promo-dots',
     autoplay: true
-})
+  })
   $('.owl-carousel--quotes').owlCarousel({
     loop:true,
     items:1,
-    nav: true,
+    dots: false,
+    nav: false,
     navText: "",
     dotsContainer: '#quotes-dots',
     navContainer: '#quotes-arrows',
-    autoplay: true
-})
+    autoplay: true,
+    responsive : {
+    720 : {
+        dots : true,
+        nav : true
+    }}
+  })
 });
